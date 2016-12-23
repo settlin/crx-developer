1919ac644cdde5362032c2f2a4e86e5187547c5d
Sudhanshu <sudhanshu@settlin.in>
2016-12-23 18:50:49 +0530

    web links

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  var url = tab.url.replace('https://portal.settlin.in', 'http://localhost:3000');
+  var url = tab.url.replace('https://portal.settlin.in', 'http://localhost:3000').replace('https://settlin.in', 'http://localhost:4000');
   console.log(url);
   chrome.tabs.executeScript({
     code: 'location.href="' + url + '"'
