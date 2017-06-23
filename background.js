75f32c85260fdf9c9c178f5633b969351ad55f5d
Sudhanshu <sudhanshu@settlin.in>
2017-06-23 19:25:15 +0530

    bump version

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
+var pairs = {
+	'https://portal.settlin.in': 'http://localhost:3000',
+	'http://localhost:3000': 'http://localhost:4000',
+	'http://localhost:4000': 'https://settlin.in',
+	'https://settlin.in': 'https://portal.settlin.in',
+};
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  var url = tab.url.replace('https://portal.settlin.in', 'http://localhost:3000').replace('https://settlin.in', 'http://localhost:4000');
-  console.log(url);
-  chrome.tabs.executeScript({
-    code: 'location.href="' + url + '"'
-  });
+	for (var url in pairs) {
+		if (pairs.hasOwnProperty(url)) {
+			if (tab.url.startsWith(url)) {
+				chrome.tabs.executeScript({
+					code: 'location.href="' + tab.url.replace(url, pairs[url]) + '"'
+				});
+			}
+		}
+	}
 });
